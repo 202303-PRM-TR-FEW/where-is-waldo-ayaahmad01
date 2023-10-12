dd6d624de720371dd416274c2194a0ffc9b637e8
Aya Abu AlHummos <ayaahmad113@gmail.com>
2023-10-12 19:14:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-const example = [
-  ["A", "A", "A"],
-  ["A", "A", "A"],
-  ["A", "B", "A"]];
+function whereIsWaldo(matrix) {
+  if (!Array.isArray(matrix) || matrix.length < 2) {
+    return null; 
+  }
 
-const whereIsWaldo = (matrix) => {
-  
+  const referenceElement = matrix[0][0]; 
+
+  for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+      if (matrix[row][col] !== referenceElement) {
+        return [row + 1, col + 1];
+      }
+    }
+  }
+
+  return null; 
 }
+
+const matrix = [
+  ["c", "c", "c", "c"],
+  ["c", "c", "c", "d"]
+];
+
+const differingElementCoordinates = whereIsWaldo(matrix);
+console.log(differingElementCoordinates);
